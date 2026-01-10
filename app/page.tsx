10bd020a97a70604ba2e095b11246fa0025d5d3b
mohammed-dhabaab <mohammed.dhabaab@gmail.com>
2026-01-10 13:21:07 +0300

    feat: develop the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,49 @@
+"use client";
+
+import BankAccounts from "./components/BankAccounts";
+import Cards from "./components/Cards";
+import Heading from "./components/Heading";
+import Navbar from "./components/Navbar";
+import Paragraph from "./components/Paragraph";
+import SharedData from "./components/SharedData";
+import { infoIcon } from "./assets/icons";
+import Button from "./components/Button";
 import Image from "next/image";
+import { useBankAccountsStore } from "./stores/bankAccountsStore";
+import { useCardsStore } from "./stores/cardsStore";
 
 export default function Home() {
+  const { selectedBankAccounts } = useBankAccountsStore();
+  const { selectedCards } = useCardsStore();
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <main className="min-h-screen bg-main-background text-text-main">
+      <div className="max-w-300 mx-auto flex flex-col px-4 py-8">
+        <Navbar />
+        <Heading as="h1" size="xl" className="mt-6 font-semibold">Connect with Malaa</Heading>
+        <Paragraph size="md" className="text-text-secondary mt-2 mb-4">Select accounts and cards to share details</Paragraph>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          <BankAccounts />
+          <Cards />
+          <SharedData />
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        <div className="flex flex-col gap-4 mt-4">
+          <Paragraph size="sm" className="bg-background-secondary p-3 rounded-xl font-semibold sm:w-fit">Your data will be shared starting from 16/12/2024 until 16/12/2025. </Paragraph>
+          <Paragraph size="sm" className="text-text-secondary">Malaa will have ongoing access to your data for 12 months. You can revoke these permissions at any time from the Settings. </Paragraph>
+          <div className="flex items-start md:items-center gap-2 p-3 bg-background-secondary rounded-lg mt-2 sm:w-fit">
+            <Image src={infoIcon} alt="info icon" className="w-4 h-4 mt-1 md:mt-0" />
+            <Paragraph size="sm" className="font-bold ">By proceeding, you agree to our <span className="text-primary">terms & conditions</span> and will be securely redirected to Malaa.</Paragraph>
+          </div>
+
+          <div className="flex justify-end gap-2">
+            <Button variant={"danger"} size={"lg"}>Deny</Button>
+            <Button disabled={selectedBankAccounts?.length === 0 || selectedCards?.length === 0} variant={"secondary"} size={"lg"}>Allow</Button>
+          </div>
         </div>
-      </main>
-    </div>
+      </div>
+
+    </main>
   );
 }
