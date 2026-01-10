d6360127e44566872066c623892b1907c8093bab
mohammed-dhabaab <mohammed.dhabaab@gmail.com>
2026-01-10 13:20:27 +0300

    feat: add project assets

---
+++ app/assets/icons/index.ts
@@ -0,0 +1,27 @@
+import primaryAccountIcon from "./primary-account-icon.svg"
+import subAccount1Icon from "./sub-account-icon-1.svg"
+import subAccount2Icon from "./sub-account-icon-2.svg"
+import pettyCardIcon from "./petty-card-icon.svg"
+import cashbackCardIcon from "./cashback-card-icon.svg"
+import arrowUpIcon from "./arrow-up-icon.svg"
+import bankIcon from "./bank-icon.svg"
+import hamburgerMenuIcon from "./hamburger-menu-icon.svg"
+import infoIcon from "./info-icon.svg"
+import loopTimeIcon from "./loop-time-icon.svg"
+import twoArrowsIcon from "./two-arrows-icon.svg"
+import twoPeopleIcon from "./two-people-icon.svg"
+
+export {
+    primaryAccountIcon,
+    subAccount1Icon,
+    subAccount2Icon,
+    arrowUpIcon,
+    bankIcon,
+    pettyCardIcon,
+    cashbackCardIcon,
+    hamburgerMenuIcon,
+    infoIcon,
+    loopTimeIcon,
+    twoArrowsIcon,
+    twoPeopleIcon,
+}
